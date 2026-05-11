14ba6d958f775018fe7caa928c761714e509f0db
Taryn Papas <taryn.papas@gmail.com>
2026-05-11 16:36:23 +1000

    update

--- booking-form.js
+++ booking-form.js
@@ -18,7 +18,7 @@ form.addEventListener("submit", async (e) => {
     };
 
     try {
-        const response = await fetch("/.netlify/functions/send-email", {
+        const response = await fetch("/netlify/functions/send-email.js", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
